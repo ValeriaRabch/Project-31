2795f79837a1ae2f215ac9352a1dd3957ded186a
ValeriaRabch <rabchenukv@gmail.com>
2024-04-09 10:45:40 +0300

    Add method Print

--- Source.cpp
+++ Source.cpp
@@ -15,9 +15,39 @@ public:
 		month = m;
 		year = y;
 	}
+	Date operator + (int number) {
+		Date data2;
+
+		int days = day + (month * 30) + (year * 365);
+		days += number;
+		data2.year = days / 365; days -= year * 365;
+		data2.month = days / 30; days -= month * 30;
+		if (days < 30) {
+			data2.day = days;
+		}
+		else {
+			for (int i = days; ; i -= 30) {
+				if (i < 30) {
+					data2.day = i;
+					break;
+				}
+			}
+		}
+
+		return data2;
+	}
+	void Print() {
+		cout << "Day " << day << endl;
+		cout << "Month " << month << endl;
+		cout << "Year " << year << endl;
+	}
 };
 
 int main() {
+	Date a(9, 4, 2024), b;
+
+	b = a + 32;
+	b.Print();
 
 	return 0;
 }
